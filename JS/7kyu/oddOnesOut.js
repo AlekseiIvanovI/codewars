bfd1b2bb31711928f395925b7933f86e2ce4a216
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2024-02-05 20:01:16 -0800

    new commit

---
+++ JS/7kyu/oddOnesOut.js
@@ -0,0 +1,18 @@
+// Challenge: You are given a list of numbers. The numbers each repeat a certain number of times. 
+//Remove all numbers that repeat an odd number of times while keeping everything else the same.
+// oddOnesOut([1, 2, 3, 1, 3, 3]) = [1, 1]
+// In the above example:
+// the number 1 appears twice
+// the number 2 appears once
+// the number 3 appears three times
+// 2 and 3 both appear an odd number of times, so they are removed from the list. The final result is: [1,1]
+
+//Solution
+function oddOnesOut(nums) {
+    const countMap = {};
+
+    nums.forEach(num => {
+        countMap[num] = (countMap[num] || 0) + 1;
+    });
+    return nums.filter(num => countMap[num] % 2 === 0);
+}
